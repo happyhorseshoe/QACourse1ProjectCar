38fce5983884770de623b825925e5579d9b806c3
Elizabeth Morrison <emorrison24@gmail.com>
2022-11-15 23:02:15 -0500

    first draft semitrucktest

--- CodeLouisvilleUnitTestProjectTests/SemiTruckTests.cs
+++ CodeLouisvilleUnitTestProjectTests/SemiTruckTests.cs
@@ -1,10 +1,11 @@
 ﻿using CodeLouisvilleUnitTestProject;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
 
 namespace CodeLouisvilleUnitTestProjectTests
 {
-    public class SemiTruckTests
+    public class SemiTruckTestsShould
     {
 
         //Verify that the SemiTruck constructor creates a new SemiTruck
@@ -15,34 +16,54 @@ namespace CodeLouisvilleUnitTestProjectTests
         public void NewSemiTruckIsAVehicleAndHas18TiresAndEmptyCargoTest()
         {
             //arrange
-            throw new NotImplementedException();
+            SemiTruck semiTruck = new SemiTruck();
             //act
 
             //assert
-            
-        }
+            using (new AssertionScope())
+            {
+                semiTruck.Should().Be(semiTruck);
+                semiTruck.Should().BeOfType<Vehicle>();
+                semiTruck.NumberOfTires.Should().Be(18);    //is this redundant?
+                semiTruck.Cargo.Should().BeEmpty().And.NotBeNull();    //can I do this or needs to be sep?
 
-        //Verify that adding a CargoItem using LoadCargo does successfully add
-        //that CargoItem to the Cargo. Confirm both the existence of the new
-        //CargoItem in the Cargo and also that the count of Cargo increased to 1.
-        [Fact]
-        public void LoadCargoTest()
-        {
-            //arrange
-            throw new NotImplementedException();
-            //act
 
-            //assert
+            }
 
-        }
+            //Verify that adding a CargoItem using LoadCargo does successfully add
+            //that CargoItem to the Cargo. Confirm both the existence of the new
+            //CargoItem in the Cargo and also that the count of Cargo increased to 1.
+
+            [Fact]
+            public void LoadCargoTest()
+            {
+                //arrange
+              CargoItem crate = new CargoItem();
+              SemiTruck semiTruck = new SemiTruck();
+                
+
+                //act
+              semiTruck.LoadCargo(crate);
+
+                //assert
+                using (new AssertionScope())
+                {
+                    semiTruck.Cargo.Should().Contain(crate);
+                    semiTruck.Cargo.Should().HaveCountGreaterThanOrEqualTo(1);
+
+                }
+
+            }
+        
 
         //Verify that unloading a  cargo item that is in the Cargo does
         //remove it from the Cargo and return the matching CargoItem
         [Fact]
         public void UnloadCargoWithValidCargoTest()
         {
-            //arrange
-            throw new NotImplementedException();
+                //arrange
+                SemiTruck semiTruck = new SemiTruck();
+                CargoItem crate = new CargoItem();
             //act
 
             //assert
