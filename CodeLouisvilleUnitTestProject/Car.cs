903fd6710ff66418e697bdc8328f30d0a2cf4fcb
Elizabeth Morrison <emorrison24@gmail.com>
2022-11-17 22:30:43 -0500

    finished car.cs

--- CodeLouisvilleUnitTestProject/Car.cs
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -41,19 +41,50 @@ namespace CodeLouisvilleUnitTestProject
            // Make = make;
            // Model = model;
         }
+        public async Task<bool> IsValidModelForMakeAsync()
+        {
+            string urlSuffix = $"vehicles/getmodelsformake/{Make}?format=json";
+            string response = await _client.GetStringAsync(urlSuffix);
+            var data = JsonSerializer.Deserialize<ModelForMakeYearResponse>(response);
+            return data.Results.Any(r => r.Model_Name == Model);
+
+        }
 
         public async Task<bool> WasModelMadeInYearAsync(int year)
         {
             var model = this.Model;
             if (year < 1995) throw new ArgumentException("No data is available for years before 1995");
-            string urlSuffix = $"/vehicles/getmodelsformakeyear/make/{Make}/modelyear/{year}?format=json";
+            string urlSuffix = $"vehicles/getmodelsformakeyear/make/{Make}/modelyear/{year}?format=json";
             var response = await _client.GetAsync(urlSuffix);
             var rawJson = await response.Content.ReadAsStringAsync();
             var data = JsonSerializer.Deserialize<ModelForMakeYearResponse>(rawJson);
             return data.Results.Any(r => r.Model_Name == Model);
             
         }
+        public void AddPassengers(int passengers)
+        {
+            if(passengers > 0)
+            {
+                NumberOfPassengers += passengers;
+                MilesPerGallon -= passengers * 0.2;
+            }
+        }
+        public void RemovePassengers(int passengers)
+        {
+            if(passengers >0)
 
+            {
+                int removed = passengers;
+                
+                if(passengers > NumberOfPassengers)
+                {
+                    removed = NumberOfPassengers;
+                }
+
+                NumberOfPassengers -= removed;
+                MilesPerGallon += removed * 0.2;
+            }
+        }
 
 
     }
